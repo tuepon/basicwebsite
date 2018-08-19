a1c0cd2691fe18822999b7c598b1137966802d6a
Tomonori Ueno <t.uepon@gmail.com>
2018-08-19 17:17:56 +0900

    before MessagesController

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,4 +2,21 @@
 
 @section('content')
   <h1>Contact</h1>
+  {!! Form::open(['url' => 'contact/submit']) !!}
+    <div class="form-group">
+      {{Form::label('name', 'Name')}}
+      {{Form::text('name', '', ['class' => 'form-control', 'placeholder' => 'Enter Name'])}}
+    </div>
+    <div class="form-group">
+      {{Form::label('email', 'E-Mail Address')}}
+      {{Form::text('email', '', ['class' => 'form-control', 'placeholder' => 'Enter email'])}}
+    </div>
+    <div class="form-group">
+      {{Form::label('message', 'Message')}}
+      {{Form::textarea('message', '', ['class' => 'form-control', 'placeholder' => 'Enter message'])}}
+    <div>
+      {{Form::submit('submit', ['class' => 'btn btn-primary'])}}
+    </div>
+    </div>
+  {!! Form::close() !!}
 @endsection
