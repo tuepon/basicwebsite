9f38c92b260d9a7494fd3ebfab3830f1344b6261
Tomonori Ueno <t.uepon@gmail.com>
2018-08-19 18:31:04 +0900

    validation is OK

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,12 +9,13 @@
     @include('inc.navbar')
 
     <div class="container">
-      <!-- Homepage only output -->
+      <!-- display Homepage only -->
       @if(Request::is('/'))
         @include('inc.showcase')
       @endif
       <div class="row">
-        <div class="col-md-8 col-lg-8">  
+        <div class="col-md-8 col-lg-8">
+          @include('inc.messages')
           @yield('content')
         </div>  
         <div class="col-md-4 col-lg-4">
