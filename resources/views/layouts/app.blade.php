b0ca4e0e23400e9fd68771ac181b12723d22551c
Tomonori Ueno <t.uepon@gmail.com>
2018-08-19 13:59:04 +0900

    completed to rout to all contents

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>Basic Website</title>
+</head>
+<body>
+  @yield('content')
+
+  @include('inc.sidebar')
+
+</body>
+</html>
